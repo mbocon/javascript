e1dfbde9a6e3bfa119640fcbe253c93b82924c7f
Michael Bocon <mikebocon@gmail.com>
2021-02-18 23:32:28 -1000

    game finished

---
+++ tic_tac_toe/script.js
@@ -0,0 +1,85 @@
+// creating the players
+const playerOne = 'O';
+const playerTwo = 'X';
+
+// getting restart button
+let restartBtn = document.getElementById('restartBtn')
+
+//playerOne goes first
+let currentPlayer = playerOne;
+
+// creating the array to hold taken positions on the gameboard
+const spaces = [null, null, null, null, null, null, null, null, null];
+
+const boxClicked = id => {
+	if (!spaces[id]) {
+// IF THE SPACES ARRAY DOES NOT CONTAIN AN X OR O 
+// THEN ASSIGN THAT SPACE TO THE CURRENT PLAYER
+		spaces[id] = currentPlayer;
+		let selected = document.getElementById(id);
+		selected.append(currentPlayer);
+// CHECK TO SEE IF A PLAYER HAS WON
+		playerHasWon();
+// ALTERNATE THE PLAYERS AFTER EACH TURN
+		currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+	}
+};
+
+//  CHECK ALL POSSIBLE  WINNING COMBINATIONS
+const playerHasWon = () => {
+	if (spaces[0] === currentPlayer) {
+		if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	}
+	if (spaces[0] === currentPlayer) {
+		if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	}
+	if (spaces[0] === currentPlayer) {
+		if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	} else if (spaces[8] === currentPlayer) {
+		if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	} else if (spaces[8] === currentPlayer) {
+		if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	} else if (spaces[2] === currentPlayer) {
+		if (spaces[4] === currentPlayer && spaces[6] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	} else if (spaces[3] === currentPlayer) {
+		if (spaces[4] === currentPlayer && spaces[5] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	} else if (spaces[1] === currentPlayer) {
+		if (spaces[4] === currentPlayer && spaces[7] === currentPlayer) {
+			console.log(`${currentPlayer} wins!`);
+			let diplayWinner = document.getElementById('gameText');
+			diplayWinner.append(`${currentPlayer} HAS WON!`);
+		}
+	}
+};
+
+restartBtn.addEventListener('click', () => {
+    window.location.reload()
+})
