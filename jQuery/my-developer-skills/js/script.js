dd8cca71d1124f58fa642fabd6bcebdd44efe753
Michael Bocon <mikebocon@gmail.com>
2021-03-13 14:03:32 -1000

    modified js

--- jQuery/my-developer-skills/js/script.js
+++ jQuery/my-developer-skills/js/script.js
@@ -1,49 +1,49 @@
 $(function () {
-	// state variables
-	let skills, skill;
+    // state variables
+    let skills, skill;
 
-	// cached elements
 
-	const $button = $('button');
-	const $ul = $('ul');
-	const $input = $('input');
+    // cached element references
 
-	// event listeners
-	$button.on('click', handleAddSkill);
-	$ul.on('click', '.delete', handleDelete);
+    const $button = $('button');
+    const $ul = $('ul');
+    const $input = $('input');
 
-	// functions
-	init();
 
-	function init() {
-		skills = JSON.parse(localStorage.getItem('skills'));
-		render();
-	}
+    // event listeners
+    $button.on('click', handleAddSkill);
+    $ul.on('click', '.delete', handleDelete);
 
-	function handleAddSkill() {
-		skill = $input.val();
-		if (skill) {
-			skills.push(skill);
-            localStorage.setItem('skills', JSON.stringify(skills))
-            console.log(localStorage, 'is localStorage')
-			$input.val('');
-			render();
-		} else return;
-	}
 
-	function handleDelete() {
-		const $skill = $(this).closest('li');
-		$skill.fadeOut(150, function () {
-			const idx = skills.indexOf($skill.text().replace('X', ''));
-			skills.splice(idx, 1);
-            localStorage.setItem('skills', JSON.stringify(skills))
-			render();
-		});
-	}
+    // functions
+    init();
 
-	function render() {
-		if (!skills.length) $ul.css('margin-bottom', '30px');
-		else $ul.css('margin-bottom', '30px');
-		$ul.html(skills.map(skill => $(`<li><span class="delete">X</span>${skill}</li>`)));
-	}
+    function init() {
+        skills = [];
+        render();
+    }
+
+    function handleAddSkill() {
+        skill = $input.val();
+        if (skill) {
+            skills.push(skill);
+            $input.val('');
+            render();
+        } else return;
+    }
+
+    function handleDelete() {
+        const $skill = $(this).closest('li');
+        $skill.fadeOut(150, function() {
+            const idx = skills.indexOf($skill.text().replace('X', ''));
+            skills.splice(idx, 1);
+            render();
+        });
+    }
+
+    function render() {
+        if (!skills.length) $ul.css('margin-bottom', '30px');
+        else $ul.css('margin-bottom', '30px');
+        $ul.html(skills.map(skill => $(`<li><span class="delete">X</span>${skill}</li>`)));
+    }
 });
