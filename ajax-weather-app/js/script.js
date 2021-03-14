9b05f5d3775cae9207f22843f5cfb1721f6f23b0
Michael Bocon <mikebocon@gmail.com>
2021-03-13 14:02:14 -1000

    removed console.logs

--- ajax-weather-app/js/script.js
+++ ajax-weather-app/js/script.js
@@ -11,7 +11,6 @@ $(() => {
 		let $search = $input.val();
 		// clear the input form for the next search
 		$input.val('');
-		console.log($search);
 		// ajax request
 		$.ajax({
 			url: `http://api.openweathermap.org/data/2.5/weather?q=${$search}&units=imperial&appid=be853936af64e10c7ae809463ba68499`,
@@ -33,7 +32,6 @@ $(() => {
 		$temp.text('Tempurature : ' + Math.round(data.main.temp) + ' F');
 		$feels.text('Feels liks : ' + Math.round(data.main.feels_like) + ' F');
 		$weather.text('Weather : ' + data.weather[0].description);
-		console.log(data.name, data.main.feels_like, data.main.temp, data.weather[0].description);
 	}
 
 	function renderError(error) {
